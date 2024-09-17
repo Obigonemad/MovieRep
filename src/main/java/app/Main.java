81434b6c718fb66901108ecdc09f1017f4b2b1a6
SimoneSkovgaard <simonerskovgaard@gmail.com>
2024-09-17 13:47:56 +0200

    not in main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,28 +5,32 @@ import app.entities.Actor;
 import app.entities.Director;
 import app.entities.Genre;
 import app.entities.Movie;
+import app.services.MovieService1;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) {
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("moviedatabase");
-        EntityManager em = emf.createEntityManager();
+    public static void main(String[] args) throws Exception {
+//        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("moviedatabase");
+//        EntityManager em = emf.createEntityManager();
+//
+//        Actor a1 = new Actor("hansTheActor");
+//        Director d1 = new Director("JohnTheDirector");
+//        Movie m1 = new Movie("SpiderMan8", 10, LocalDate.of(2025,01,02));
+//        Genre g1 = new Genre("Action");
+//
+//
+//        em.getTransaction().begin();
+//        em.persist(a1);
+//        em.persist(d1);
+//        em.persist(m1);
+//        em.persist(g1);
+//        em.getTransaction().commit();
+//        em.close();
 
-        Actor a1 = new Actor("hansTheActor");
-        Director d1 = new Director("JohnTheDirector");
-        Movie m1 = new Movie("SpiderMan8", 10, LocalDate.of(2025,01,02));
-        Genre g1 = new Genre("Action");
-
-
-        em.getTransaction().begin();
-        em.persist(a1);
-        em.persist(d1);
-        em.persist(m1);
-        em.persist(g1);
-        em.getTransaction().commit();
-        em.close();
+        MovieService1 movieService1 = new MovieService1();
+        movieService1.fetchDanishMovies();
     }
 }
--- src/main/java/app/services/MovieService1.java
+++ src/main/java/app/services/MovieService1.java
@@ -19,13 +19,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MovieService1 {
-    private static final String API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODUyNThiYjUwOGE5MDM3MWE4NWRkMzRlZGY1ZTUyMyIsIm5iZiI6MTcyNjU2MTg0OS44NjM1MzMsInN1YiI6IjY2ZTE2Y2U4MDAwMDAwMDAwMDIyYWJjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oP59a5lAHZGHNHykBDK6DQh2UwWoN2GYQE6mE45y7KQ";
+    private static final String API_KEY = System.getenv("api_key");
     private static final String BASE_URL = "https://api.themoviedb.org/3";
 
-    public static void main(String[] args) throws Exception {
+    public static void fetchDanishMovies() throws Exception {
         HttpClient client = HttpClient.newHttpClient();
 
-        // 1. Fetch the list of movies
+        // 1. Fetch the list of Danish movies
         String discoverMoviesUrl = BASE_URL + "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&release_date.gte=2019-09-17&sort_by=popularity.desc&with_origin_country=DK";
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(discoverMoviesUrl))
@@ -58,7 +58,6 @@ public class MovieService1 {
             System.out.println("Error: " + response.statusCode());
         }
     }
-
     // Method to fetch credits using the movie ID
     private static void fetchMovieCredits(HttpClient client, String movieId) throws Exception {
         String creditsUrl = BASE_URL + "/movie/" + movieId + "/credits?language=da";
